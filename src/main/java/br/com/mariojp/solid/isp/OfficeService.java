432a716c89e1819c1c2c883f347e6e8e15699e97
Raphael Maurício <raphaelmauricio12@gmail.com>
2025-08-26 14:27:51 -0300

    [FIX] Removendo método não usado em printReport

--- src/main/java/br/com/mariojp/solid/isp/OfficeService.java
+++ src/main/java/br/com/mariojp/solid/isp/OfficeService.java
@@ -1,11 +1,9 @@
 package br.com.mariojp.solid.isp;
 
 public class OfficeService {
-    private final MultiFunctionDevice device;
-    public OfficeService(MultiFunctionDevice device){ this.device = device; }
+    private final Printer device;
+    public OfficeService(Printer device){ this.device = device; }
     public void printReport(String content){
         device.print(content);
-        // BUG: chama scan sem necessidade
-        device.scan("audit.txt");
     }
 }

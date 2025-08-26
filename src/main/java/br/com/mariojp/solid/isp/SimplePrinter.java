24d551d331f0ac61530cbb864ee6f966e8c17ef7
Raphael Maurício <raphaelmauricio12@gmail.com>
2025-08-26 14:27:29 -0300

    Fazendo SimplePrinter implementar a interface Printer

--- src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
+++ src/main/java/br/com/mariojp/solid/isp/SimplePrinter.java
@@ -1,6 +1,5 @@
 package br.com.mariojp.solid.isp;
 
-public class SimplePrinter implements MultiFunctionDevice {
+public class SimplePrinter implements Printer {
     @Override public void print(String content){ /* imprime */ }
-    @Override public void scan(String targetFile){ throw new UnsupportedOperationException("Sem scanner"); }
 }
